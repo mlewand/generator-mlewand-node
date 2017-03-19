5c07c93be53fba6960ac464d1b8d34891b67b46b
Marek Lewandowski <marek@mlewandowski.com>
2017-03-19 16:38:18 +0100

    Added more tests.

--- test/app.js
+++ test/app.js
@@ -2,17 +2,19 @@
 
 const path = require( 'path' ),
 	helpers = require( 'yeoman-test' ),
-	assert = require( 'assert' );
+	assert = require( 'assert' ),
+	yoAssert = require( 'yeoman-assert' );
 
 describe( 'generator-mlewand-node:app', function() {
-	describe( 'generator-node integration', function() {
-		this.timeout( 5000 );
+	this.timeout( 5000 );
 
+	describe( 'generator-node integration', function() {
 		let pkg,
 			tmpDir;
 
 		before( () => helpers.run( path.join( __dirname, '../generators/app' ) )
 			.withPrompts( { license: 'MIT' } )
+			.inTmpDir()
 			.then( dir => {
 				tmpDir = dir;
 				pkg = require( path.join( tmpDir, 'package.json' ) );
@@ -26,5 +28,20 @@ describe( 'generator-mlewand-node:app', function() {
 		it( 'adds good stuff to package.json', () => {
 			assert.strictEqual( true, 'eslint-config-mlewand-node' in pkg.devDependencies, 'eslint-config-mlewand-node was not added' );
 		} );
+	} );
+
+
+	it( 'doesnt add AppVeyor if not needed', () => {
+		return helpers.run( path.join( __dirname, '../generators/app' ) )
+			.withPrompts( {
+				license: 'MIT'
+			} )
+			.withOptions( {
+				skipAppveyor: true
+			} )
+			.inTmpDir()
+			.then( () => {
+				return yoAssert.noFile( '.appveyor.yml' );
+			} );
 	} );
 } );
