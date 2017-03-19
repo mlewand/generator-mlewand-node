4367a7f0d84a515438bee077923af65fd6bd343c
Marek Lewandowski <marek@mlewandowski.com>
2017-03-19 12:33:54 +0100

    Got rid of eslint-config-xo-space dependency, add ESLint config based on .eslintrc files.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,17 +1,51 @@
 'use strict';
 
-const Generator = require( 'yeoman-generator' );
+const Generator = require( 'yeoman-generator' ),
+	extend = require( 'deep-extend' );
 
 module.exports = class extends Generator {
 	default() {
 		this.composeWith( require.resolve( 'generator-node/generators/app' ), {
 			editorconfig: false,
 			githubAccount: 'mlewand',
-			skipInstall: true
+			skipInstall: true,
+			license: this.options.license || 'MIT',
+			coveralls: false,
+			babel: false
 		} );
 	}
 
-	extendPackage() {
+	writing() {
+		this.fs.copy( this.templatePath( '**/.eslintrc' ), this.destinationPath() );
+	}
+
+	/*
+	Haxes, haxes detected:
+	I couldn't disable node:eslint task which was injecting eslint-config-xo-space dependency.
+	Also my `writing` method was ran __before__ composite tasks` writing calls, so it was showing an empty file.
+	As a result I had to hook up to a later priority.
+	*/
+	conflicts() {
+		const pkg = this.fs.readJSON( this.destinationPath( 'package.json' ), {} ),
+			generatorGeneratorPkg = require( '../../package.json' );
+
+		// Remove eslint in package.json, it's better to keep it as a file.
+		delete pkg.eslintConfig;
+
+		// Couple of modules should be removed.
+		if ( pkg.devDependencies ) {
+			delete pkg.devDependencies[ 'eslint-config-xo-space' ];
+		}
+
+		extend( pkg, {
+			devDependencies: {
+				'eslint-config-mlewand-node': generatorGeneratorPkg.devDependencies[ 'eslint-config-mlewand-node' ]
+			}
+		} );
+
+		pkg.keywords = pkg.keywords || [];
+
+		this.fs.writeJSON( this.destinationPath( 'package.json' ), pkg );
 	}
 
 	install() {
--- test/app.js
+++ test/app.js
@@ -1,12 +1,30 @@
 'use strict';
 
 const path = require( 'path' ),
-	helpers = require( 'yeoman-test' );
+	helpers = require( 'yeoman-test' ),
+	assert = require( 'assert' );
 
 describe( 'generator-mlewand-node:app', function() {
-	before( function() {
-		return helpers.run( path.join( __dirname, '../generators/app' ) )
+	describe( 'generator-node integration', function() {
+		this.timeout( 5000 );
+
+		let pkg,
+			tmpDir;
+
+		before( () => helpers.run( path.join( __dirname, '../generators/app' ) )
 			.withPrompts( { license: 'MIT' } )
-			.toPromise();
+			.then( dir => {
+				tmpDir = dir;
+				pkg = require( path.join( tmpDir, 'package.json' ) );
+			} )
+		);
+
+		it( 'clears package.json produced by base generator', () => {
+			assert.strictEqual( false, 'eslint-config-xo-space' in pkg.devDependencies, 'eslint-config-xo-space is not removed' );
+		} );
+
+		it( 'adds good stuff to package.json', () => {
+			assert.strictEqual( true, 'eslint-config-mlewand-node' in pkg.devDependencies, 'eslint-config-mlewand-node was not added' );
+		} );
 	} );
 } );
